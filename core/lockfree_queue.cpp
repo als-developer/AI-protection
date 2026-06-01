cf2579be8a937a37bd4809093a6a6b37e87eaedc
ALS <zawadisanga397@gmail.com>
2026-06-01 21:15:14 +0300

    Create lockfree_queue.cpp

---
+++ core/lockfree_queue.cpp
@@ -0,0 +1,50 @@
+#include <atomic>
+#include <cstddef>
+#include "lockfree_queue.h"
+
+LockFreeQueue::LockFreeQueue(size_t capacity) 
+    : m_capacity(capacity)
+    , m_buffer(new TelemetryPacket[capacity])
+    , m_write_index(0)
+    , m_read_index(0)
+{
+    // Ensure capacity is power of two for fast modulo
+    m_capacity = 1;
+    while (m_capacity < capacity) m_capacity <<= 1;
+}
+
+LockFreeQueue::~LockFreeQueue() {
+    delete[] m_buffer;
+}
+
+bool LockFreeQueue::push(const TelemetryPacket& packet) {
+    size_t write_idx = m_write_index.load(std::memory_order_relaxed);
+    size_t read_idx = m_read_index.load(std::memory_order_acquire);
+    
+    if (write_idx - read_idx >= m_capacity) {
+        return false; // Queue full
+    }
+    
+    m_buffer[write_idx & (m_capacity - 1)] = packet;
+    m_write_index.store(write_idx + 1, std::memory_order_release);
+    return true;
+}
+
+bool LockFreeQueue::pop(TelemetryPacket& packet) {
+    size_t read_idx = m_read_index.load(std::memory_order_relaxed);
+    size_t write_idx = m_write_index.load(std::memory_order_acquire);
+    
+    if (read_idx == write_idx) {
+        return false; // Queue empty
+    }
+    
+    packet = m_buffer[read_idx & (m_capacity - 1)];
+    m_read_index.store(read_idx + 1, std::memory_order_release);
+    return true;
+}
+
+size_t LockFreeQueue::size() const {
+    size_t write_idx = m_write_index.load(std::memory_order_acquire);
+    size_t read_idx = m_read_index.load(std::memory_order_acquire);
+    return write_idx - read_idx;
+}
